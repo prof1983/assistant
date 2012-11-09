0f861930d36d2b0780960522ae949457e8a34276
Prof1983 <prof1983@yandex.ru>
2012-11-09 13:46:25 +0600

    Added DelphiXE2 files

---
+++ DelphiXE2/Assistant.dpr
@@ -0,0 +1,16 @@
+{**
+@Abstract Personal assistant
+@Author Prof1983 <prof1983@ya.ru>
+@Created 03.07.2007
+@LastMod 17.08.2012
+}
+program Assistant;
+
+uses
+  AssistantBoot in '..\AssistantBoot.pas';
+
+{$R *.res}
+
+begin
+  Assistant_Boot();
+end.
